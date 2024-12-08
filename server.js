24045c6281bfb4022464d330e8c397340340e10a
sajanchahal <113524537+sajanchahal@users.noreply.github.com>
2024-12-08 11:50:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,37 @@ app.get("/", (req, res) => {
             <br>
             <button type="submit">Upload</button>
         </form>
+        <hr>
+        <div id="status">Waiting for upload...</div>
+        <script>
+            const eventSource = new EventSource('/status');
+            eventSource.onmessage = (event) => {
+                document.getElementById('status').innerText = event.data;
+            };
+        </script>
     `);
+});
+
+// Live status updates
+let statusMessage = "Waiting for upload...";
+
+app.get("/status", (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+
+    const sendStatus = () => {
+        res.write(`data: ${statusMessage}\n\n`);
+    };
+
+    sendStatus();
+
+    // Interval to keep connection alive
+    const interval = setInterval(sendStatus, 1000);
+
+    req.on("close", () => {
+        clearInterval(interval);
+    });
 });
 
 // Handle file upload
@@ -42,6 +72,7 @@ app.post("/upload", async (req, res) => {
         const totalSize = parseInt(headResponse.headers["content-length"], 10);
 
         if (isNaN(totalSize)) {
+            statusMessage = "Error: Unable to determine file size.";
             return res.status(400).send("Unable to determine file size. Make sure the URL is valid.");
         }
 
@@ -62,29 +93,38 @@ app.post("/upload", async (req, res) => {
                 response.data.on("data", (chunk) => {
                     uploadedSize += chunk.length; // Update uploaded size
                     const percent = ((uploadedSize / totalSize) * 100).toFixed(2);
-                    process.stdout.write(`\rUploading... ${percent}% (${uploadedSize}/${totalSize} bytes)`);
+                    statusMessage = `Uploading... ${percent}% (${uploadedSize}/${totalSize} bytes)`;
+                });
+
+                response.data.on("end", () => {
+                    statusMessage = "Upload complete!";
                 });
 
                 client.put(response.data, destination, (err) => {
                     if (err) {
+                        statusMessage = "Failed to upload file to FTP.";
                         res.status(500).send("Failed to upload file to FTP.");
                     } else {
-                        res.send(`File successfully uploaded to FTP! (${filename})`);
+                        statusMessage = "File successfully uploaded to FTP!";
+                        res.send("File successfully uploaded to FTP!");
                     }
                     client.end();
                 });
             } catch (downloadErr) {
+                statusMessage = "Error fetching file: " + downloadErr.message;
                 res.status(500).send("Error fetching file: " + downloadErr.message);
                 client.end();
             }
         });
 
         client.on("error", (err) => {
+            statusMessage = "FTP connection error: " + err.message;
             res.status(500).send("FTP connection error: " + err.message);
         });
 
         client.connect(ftpConfig);
     } catch (err) {
+        statusMessage = "Error: " + err.message;
         res.status(500).send("Error: " + err.message);
     }
 });
